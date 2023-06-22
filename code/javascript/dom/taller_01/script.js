61d45c943eab755590d433aaecfe5217d4bd9dcb
John Arguello Ruiz <134023288+johnarhub@users.noreply.github.com>
2023-06-22 18:42:18 -0500

    javascript

---
+++ code/javascript/dom/taller_01/script.js
@@ -0,0 +1,6 @@
+function cargar_elemento (){
+ var container = document.getElementById('container')
+ var h1 = document.createElement ('h1')
+ var texto = document.createTextNode('John Arguello')
+ h1.appendChild(texto)
+}

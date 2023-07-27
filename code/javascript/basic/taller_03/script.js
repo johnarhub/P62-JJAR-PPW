eeef5d5eb9fe582daa451c0cd0aa8e3c0a21e86d
John Arguello Ruiz <134023288+johnarhub@users.noreply.github.com>
2023-07-26 21:43:37 -0500

    Javascript Básico

---
+++ code/javascript/basic/taller_03/script.js
@@ -0,0 +1,10 @@
+var texto= 'Universidad Politécnica Salesiana'
+function imprimir(){
+    let texto2 = 'Programación y Plataformas Web' //let.- crea variables dentro de funciones
+    console.log(texto2)
+    console.log(texto)
+}
+
+console.log(texto)
+
+console.log(texto2)
